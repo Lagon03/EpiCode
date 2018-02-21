6965d7b4654b355090e0eb6fee98778412acaf35
Lucas Martin <lucas.martin@epita.fr>
2018-02-21 22:18:33 +0100

    Update Encoding / Addition of H correction

--- Encoding/main.c
+++ Encoding/main.c
@@ -131,21 +131,6 @@ int main (int argc, char* argv[])
     }
     //weave->size = weave->size + Remainder_bits[data->version];
 
-    printf("-----");
-    for(int i = 0; i < QrCode->size;++i)
-        printf("%2i", i);
-    printf("\n");
-    for(size_t x = 0; x < QrCode->size; ++x) {
-        printf("%4li [", x);
-        for(size_t y = 0; y < QrCode->size; ++y) {
-            if(QrCode->mat[x][y] == 0)
-                printf("  ");
-            else
-                printf("%c ", QrCode->mat[x][y]);
-        }
-        printf("]\n");
-    }
-
     fill_mat(QrCode->mat, QrCode->size, data->version, weave_trans, (weave->size
                 * 8) + Remainder_bits[data->version]);
 
@@ -178,15 +163,15 @@ int main (int argc, char* argv[])
     }
 
     for(size_t x = 0; x < QrCode->size; ++x) {
-        printf("[");
+        /*printf("[");
         for(size_t y = 0; y < QrCode->size; ++y) {
             if(QrCode->mat[x][y] == 0)
                 printf("  ");
             else
                 printf("%c ", QrCode->mat[x][y]);
-        }
+        }*/
         free(QrCode->mat[x]);
-        printf("]\n");
+        //printf("]\n");
     }
     free(QrCode->mat);
     free(QrCode);
--- reedsolomon/test.c
+++ reedsolomon/test.c
@@ -15,10 +15,10 @@ int main()
     //}
   uint8_t x = 12;
   uint8_t y = 10;
-  uint8_t res1 = gf_mul(x,y,gf_table);
+  uint8_t res1 = gf_mul(x, y, gf_table);
   uint8_t res2 = gf_div(x, y, gf_table);
-  printf("res1: %u", res1);
-  printf("res2: %u", res2);
+  printf("res1: %u\n", res1);
+  printf("res2: %u\n", res2);
   //for(int i =0; i < LENGTH(gf_table->gf_log); i++)
   //  printf("elem %d: %u", i, gf_table->gf_log[i]);
   struct Array *msg_in = malloc(sizeof(struct Array));
@@ -45,7 +45,8 @@ int main()
   
   msg = rs_encode_msg(msg_in, 10, gf_table);
   for(int i =0; i < msg->used; i++)
-    printf("\n%u ,", msg->array[i]);
+    printf("\n%u", msg->array[i]);
+  printf("\n");
   
   return 0;
 }
