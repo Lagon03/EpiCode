422079f67ab1a1e606b138b64b088707007ae6a9
Lagon03 <stanislas.sokolov@epita.fr>
2018-02-21 18:23:00 +0100

    encoding on the rails

--- Encoding/gen_img/gen_img.c
+++ Encoding/gen_img/gen_img.c
@@ -43,14 +43,14 @@ void Generate_QrCode(char **mat, int version, const char* name, int mod_size)
     SDL_Surface *img = create_image(size);
     white_map(img);
     
-    for(int y = mod_size * 4; y / mod_size < mat_size; y += mod_size)
+    for(int y = mod_size * 4; y / mod_size - 4 < mat_size; y += mod_size)
     {
-        for(int x = mod_size * 4; x / mod_size < mat_size; x += mod_size)
+        for(int x = mod_size * 4; x / mod_size - 4 < mat_size; x += mod_size)
         {
             //warn("2");
             //warn("y %d, x %d", y, x);
             //warn("y/mod_size = %d ||x/modsize = %d", y/mod_size, x/mod_size);
-            if(mat[y/mod_size][x/mod_size] == '1')
+            if(mat[y/mod_size - 4][x/mod_size - 4] == '1')
             {
                 fill_black(img, x, y, mod_size);
             }
