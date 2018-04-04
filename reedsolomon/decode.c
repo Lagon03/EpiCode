0f6b76e89d3c8a1de429a65018e8ac575dd727be
Jack Chouchani <jack.chouchani@epita.fr>
2018-04-04 23:52:24 +0200

    FINAL

--- reedsolomon/decode.c
+++ reedsolomon/decode.c
@@ -26,7 +26,6 @@ struct Array* rs_calc_syndromes(struct Array *msg, uint8_t nsym, struct gf_table
 		insertArray(res);
 	}
 	//(else we can end up with weird calculations sometimes)
-	free(synd);
 	return res;
 }
 
@@ -64,7 +63,8 @@ struct Array* rs_find_errdata_locator(struct Array *e_pos, struct gf_tables *gf_
 		struct Array *add = gf_poly_add(one, arr);
 		e_loc = gf_poly_mul(e_loc, add, gf_table);
 	}
-	return e_loc;}
+	return e_loc;
+}
 
 /*Compute the error (or erasures if you supply sigma=erasures locator polynomial, or errata) evaluator
  polynomial Omega from the syndrome and the error/erasures/errata locator Sigma*/
@@ -125,9 +125,9 @@ struct Array* rs_correct_errdata(struct Array *msg_in, struct Array *synd, struc
 		uint8_t err_loc_prime = 1;
 		for(size_t k = 0; k < err_loc_prime_tmp->used; k++)
 			err_loc_prime = gf_mul(err_loc_prime, err_loc_prime_tmp->array[k], gf_table);
-		err_eval = reverse_arr(err_eval);
 		
-		uint8_t y = gf_poly_eval(err_eval, Xi_inv, gf_table);
+		//err_eval = reverse_arr(err_eval);
+		uint8_t y = gf_poly_eval(reverse_arr(err_eval), Xi_inv, gf_table);
 		y = gf_mul(gf_pow(X->array[i], 1, gf_table), y, gf_table);
 		uint8_t magnitude = gf_div(y, err_loc_prime, gf_table);
 		E->array[err_pos->array[i]] = magnitude;
@@ -147,30 +147,29 @@ struct Array* rs_find_error_locator(struct Array* synd, uint8_t nsym, uint8_t er
 	old_loc->array[0] = 1;
 	insertArray(old_loc);
 	
-	int synd_shift = 0;
+	unsigned int synd_shift = 0;
 	if(synd->used > nsym)
 		synd_shift = synd->used - nsym;
+	
 	for(int i = 0; i < nsym-erase_count;i++){
 		int K = i + synd_shift;
 		uint8_t delta = synd->array[K];
 		for(size_t j = 1 ; j < err_loc->used; j++){
-			struct Array *rev = malloc(sizeof(struct Array*));
-			initArray(rev, err_loc->used);
-			memmove(rev->array, err_loc->array, err_loc->used);
-			rev = reverse_arr(rev);
-			delta ^= gf_mul(rev->array[j - 1], synd->array[K - j], gf_table);
+			delta ^= gf_mul(err_loc->array[err_loc->used - (j+1)], synd->array[K - j], gf_table);
 		}
 		old_loc->array[old_loc->used + 1] = 0;
 		insertArray(old_loc);
 		if(delta != 0){
 			if(old_loc->used > err_loc->used){
 				struct Array *new_loc = malloc(sizeof(struct Array*));
-				initArray(new_loc, old_loc->size);
 				new_loc = gf_poly_scale(old_loc, delta, gf_table);
 				old_loc = gf_poly_scale(err_loc, gf_inverse(delta, gf_table), gf_table);
-				err_loc = copy_arr(new_loc, err_loc);
+				memmove(err_loc->array, new_loc->array, err_loc->used);
+				err_loc->used = new_loc->used;
 			}
-			err_loc = gf_poly_add(err_loc, gf_poly_scale(old_loc, delta, gf_table));
+			struct Array *scale = malloc(sizeof(struct Array*));
+			scale = gf_poly_scale(old_loc, delta, gf_table);
+			err_loc = gf_poly_add(err_loc, scale);
 		}
 	}
 	while(err_loc->used && err_loc->array[0] == 0)
@@ -186,12 +185,14 @@ struct Array* rs_find_error_locator(struct Array* synd, uint8_t nsym, uint8_t er
 struct Array* rs_find_errors(struct Array *err_loc, size_t nmess, struct gf_tables *gf_table)//nmess is len(msg_in)
 {
 	size_t errs = err_loc->used - 1;
+	size_t counter = 0;
 	struct Array *err_pos = malloc(sizeof(struct Array*));
 	initArray(err_pos, errs);
 	for(size_t i = 0; i < nmess; i++){
 		if(gf_poly_eval(err_loc, gf_pow(2, i, gf_table), gf_table) == 0){
-			err_pos->array[i] = nmess-1-i;
+			err_pos->array[counter] = nmess - 1 - i;
 			insertArray(err_pos);
+			counter++;
 		}
 	}
 	if(err_pos->used != errs){
