e08fd07d8226763bf1f559730337a8a2431d8efb
Lucas Martin <lucas.martin@epita.fr>
2018-05-21 14:49:32 +0200

    Debug test

--- Encoding/msg_decoding/decode.c
+++ Encoding/msg_decoding/decode.c
@@ -104,75 +104,103 @@ char* decode(char* input, int version, int level)
     //-------------------------------------------------------------------------
 
     size_t l_data = TOTAL_DECC[level][version]; // length of the data codewords
-    
+    size_t c_data = ECC_CODEWORDS_PER_BLOCK[level][version];
+
     char* data = malloc(((l_data  * 8) + 1) * sizeof(char));
     data[0] = '\0';
 
-     for(size_t g = 0; g < DCR->size; ++g)
+    // Group, one by one
+    for(size_t g = 0; g < DCR->size; ++g)
     {
+        // Block, one by one
         for(size_t b = 0; b < DCR->group[g]->size; ++b)
         {
+            size_t nb_b = DCR->group[g]->blocks[b]->size;
+            char* buffer = malloc((((nb_b + c_data) * 8) + 1) * sizeof(char));
+            buffer[0] = '\0';
+
+            // Word, one by one
             for(size_t w = 0; w < DCR->group[g]->blocks[b]->size; ++w)
+            {           
+                strncat(buffer, DCR->group[g]->blocks[b]->words[w], 8);
+                strncat(data, DCR->group[g]->blocks[b]->words[w], 8);
+            }
+            for(size_t w = 0; w < c_data; ++w)
             {
-                strncat(data, DCR->group[g]->blocks[b]->words[w], 8);            }
-        }
-    }   
-    /*for(size_t g = 0; g < DCR->size; ++g)
-    {
-        for(size_t b = 0; b < DCR->group[g]->size; ++b)
-        {
-            for(size_t w = 0; w < (size_t)ECC_CODEWORDS_PER_BLOCK[level][version]; ++w)
+                strncat(buffer, DCR->group[g]->blocks[b]->correction[w], 8);            
+            }
+            int* check = malloc((nb_b + c_data) * sizeof(int));
+            for(size_t i = 0; i < nb_b + c_data; ++i) {
+                char* tmp = listTakeAt(buffer, i * 8, i + 8);
+                tmp = realloc(tmp, 9 * sizeof(char));
+                tmp[8] = '\0';
+                check[i] = convertToDec(tmp);
+                free(tmp);
+                //printf("%i ", check[i]);
+            }
+            //printf("\n");
+            
+            struct Array *msg = malloc(sizeof(struct Array));
+            size_t l = nb_b + c_data;
+            initArray(msg, l);
+            for(size_t i = 0; i < l; ++i)
             {
-                strncat(data, DCR->group[g]->blocks[b]->correction[w], 8);            }
+                msg->array[i] = check[i];
+                insertArray(msg);
+            }
+
+            printf("Msg in: [");
+            for (size_t i = 0; i < msg->used - 1; ++i) 
+            {
+                printf("%u,", msg->array[i]);
+            }
+            printf("%u]\n", msg->array[msg->used-1]);
+
+            msg->array[5] = 0;
+            struct gf_tables *gf_table = malloc(sizeof(struct gf_tables));
+            gf_table->gf_exp = malloc(sizeof(struct Array));
+            gf_table->gf_log = malloc(sizeof(struct Array));
+            initArray(gf_table->gf_exp, 512);
+            initArray(gf_table->gf_log, 256);
+            gf_table = init_tables();
+
+            struct Array *err_loc;
+            struct Array *synd;
+            struct Array *pos;
+            //struct Array *rev_pos;
+
+            size_t nsym = ECC_CODEWORDS_PER_BLOCK[level][version];
+            printf("nsym org = %li\n", nsym);
+            synd = rs_calc_syndromes(msg, nsym, gf_table);
+            err_loc = rs_find_error_locator(synd, nsym, 0, gf_table);
+            pos = rs_find_errors(reverse_arr(err_loc), msg->used, gf_table);
+            //rev_pos = reverse_arr(pos);
+
+            printf("err_loc used = %li\n", err_loc->used);
+            if(err_loc->used - 1 != 0)
+                msg = rs_correct_errdata(msg, synd, pos, gf_table);
+            printf("Msg out: [");
+            for (size_t i = 0; i < msg->used - 1; ++i) 
+            {   
+                printf("%u,", msg->array[i]);
+            }
+            printf("%u]\n", msg->array[msg->used-1]);
+
+            // TODO : add strncat the correct data to the data array
+            free(buffer);
+            free(check);
+
+            // Word, one by one
+            /*for(size_t w = 0; w < DCR->group[g]->blocks[b]->size; ++w)
+            {           
+                strncat(data, DCR->group[g]->blocks[b]->words[w], 8);
+            }*/
+
         }
-    }*/
-    freeCodeWords(DCR);
-    
-    
+    }
+    freeCodeWords(DCR);   
     
     printf("Data retrieve from segmentation : %s\n", data);
-    /*struct Array *msg = malloc(sizeof(struct Array));
-    size_t l = getLength(data) / 8;
-    initArray(msg, l);
-    for(size_t i = 0; i < l; ++i)
-    {
-        msg->array[i] = convertToDec(listTakeAt(data, i * 8, 8));
-        insertArray(msg);
-    }
-
-    printf("Msg in: [");
-    for (size_t i = 0; i < msg->used - 1; ++i) 
-    {
-        printf("%u,", msg->array[i]);
-    }
-    printf("%u]\n", msg->array[msg->used-1]);
-
-    struct gf_tables *gf_table = malloc(sizeof(struct gf_tables));
-    gf_table->gf_exp = malloc(sizeof(struct Array));
-    gf_table->gf_log = malloc(sizeof(struct Array));
-    initArray(gf_table->gf_exp, 512);
-    initArray(gf_table->gf_log, 256);
-    gf_table = init_tables();
-
-    struct Array *err_loc;
-    struct Array *synd;
-    struct Array *pos;
-    struct Array *rev_pos;
-
-    size_t nsym = ECC_CODEWORDS_PER_BLOCK[level][version];
-    synd = rs_calc_syndromes(msg, nsym, gf_table);
-    err_loc = rs_find_error_locator(synd, nsym, 0, gf_table);
-    pos = rs_find_errors(reverse_arr(err_loc), msg->used, gf_table);
-    rev_pos = reverse_arr(pos);
-
-    msg = rs_correct_errdata(msg, synd, pos, gf_table);
-    printf("Msg out: [");
-    for (size_t i = 0; i < msg->used - 1; ++i) 
-    {
-        printf("%u,", msg->array[i]);
-    }
-    printf("%u]\n", msg->array[msg->used-1]);*/
-
 
     char* c_mode;                           // mode : 0001 / 0010 / 0100
                                             // the mode is the first 4 byte of
